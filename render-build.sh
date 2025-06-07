d63af3ed04517f37aa78cb55bd546631e8e0ea8a
ilo415 <63481221+ilo415@users.noreply.github.com>
2025-06-07 01:32:29 -0500

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -2,5 +2,8 @@
 
 pip install -r requirements.txt
 
-# Install the Playwright browsers
-python -m playwright install --with-deps
+# Set Playwright cache location (where Render won't wipe it)
+export PLAYWRIGHT_BROWSERS_PATH=/opt/render/.cache/ms-playwright
+
+# Install the browser deps where Playwright expects them
+python -m playwright install chromium --with-deps
